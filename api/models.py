75a7e2e94aaa27f8fa2e6a072dd48627eb3a1b7e
Abraham Olaoye <abraham.program@gmail.com>
2018-12-15 22:34:16 +0000

    Add some validation to Anime and Character models
    - Character names are unique per anime.
    - Anime names must be unique globally.
    - year_ended cannot be less than year_began, but it is nullable.
    - Some more test method renaming

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from taggit.managers import TaggableManager
@@ -5,14 +6,21 @@ from taggit.managers import TaggableManager
 
 
 class Anime(models.Model):
-    name = models.CharField(max_length=100)
-    year_began = models.SmallIntegerField()
-    year_ended = models.SmallIntegerField()  # what if the anime is ongoing?
+    name = models.CharField(max_length=100, unique=True)
+    year_began = models.PositiveSmallIntegerField()
+    year_ended = models.PositiveSmallIntegerField(null=True)
     synopsis = models.TextField()
     genres = TaggableManager()
     
+    def clean(self, *args, **kwargs):
+        super().clean(*args, **kwargs)
+
+        if self.year_ended and (self.year_ended < self.year_began):
+            raise ValidationError('year_ended cannot be before year_began')
+
     def __str__(self):
         return self.name
+
 
 class Character(models.Model):
     GENDERS = [
@@ -24,6 +32,9 @@ class Character(models.Model):
     gender = models.CharField(max_length=1, choices=GENDERS)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE,
                               related_name='characters')
+
+    class Meta:
+        unique_together = ('name', 'anime')
 
     def __str__(self):
         return self.name
--- api/tests/unit/test_models.py
+++ api/tests/unit/test_models.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import NON_FIELD_ERRORS, ValidationError
+from django.db.utils import IntegrityError
 from django.test import TestCase
-from taggit.models import Tag
 
 from api.models import Anime, Character
 
@@ -15,15 +16,14 @@ class AnimeModelTestCase(TestCase):
             synopsis='Awesome anime heavily featuring themes around existentialism, ennui and loneliness.'
         )
 
-
-    def test_anime_string_representation_is_equal_to_name(self):
-       self.assertEqual(str(self.anime), 'Cowboy Bebop')
+    def test_create_anime_check_string_representation_is_equal_to_name(self):
+        self.assertEqual(str(self.anime), 'Cowboy Bebop')
 
     def test_update_anime_with_new_genres_check_database(self):
         self.anime.genres.add("action", "adventure", "existential")
         self.assertEqual(len(self.anime.genres.all()), 3)
 
-    def test_create_character_with_valid_details_check_added_to_database(self):
+    def test_add_character_with_valid_details_check_added_to_database(self):
         spike = Character(
             name='Spike Spiegel',
             description='Cool bounty hunter on a lone search for a lost love...',
@@ -33,10 +33,45 @@ class AnimeModelTestCase(TestCase):
         spike.save()
         self.assertEqual(len(self.anime.characters.all()), 1)
 
+    def test_create_with_duplicate_name_check_integrity_error_is_thrown(self):
+        self.assertRaises(
+            IntegrityError,
+            Anime.objects.create,
+            name='Cowboy Bebop',
+            year_began=1997,
+            year_ended=1998,
+            synopsis='Duplicate anime'
+        )
+
+    def test_create_with_year_ended_less_than_year_began_check_is_invalid(self):
+        anime = Anime(name='Some Anime', year_began=1997, year_ended=1990, synopsis='A synopsis')
+        try:
+            anime.full_clean()
+        except ValidationError as e:
+            self.assertEqual(e.message_dict[NON_FIELD_ERRORS],
+                             ['year_ended cannot be before year_began'])
+        else:
+            self.fail('ValidationError not raised for invalid Anime instance')
+
+    def test_create_with_null_year_ended_check_is_valid(self):
+        try:
+            Anime.objects.create(name='Some Anime', year_began=1997, year_ended=None, synopsis='A synopsis')
+        except Exception as e:
+            self.fail('year_ended should be nullable. Received error {}'.format(e))
+
 
 class CharacterModelTestCase(TestCase):
 
-    def test_character_string_representation_is_equal_to_name(self):
+    @classmethod
+    def setUpTestData(cls):
+        cls.anime = Anime.objects.create(
+            name='Cowboy Bebop',
+            year_began=1997,
+            year_ended=1998,
+            synopsis='Awesome anime heavily featuring themes around existentialism, ennui and loneliness.'
+        )
+
+    def test_create_check_character_string_representation_is_equal_to_name(self):
         spike = Character(
             name='Spike Spiegel',
             description='Cool bounty hunter on a lone search for a lost love...',
@@ -44,3 +79,14 @@ class CharacterModelTestCase(TestCase):
         )
         self.assertEqual(str(spike), 'Spike Spiegel')
 
+    def test_create_with_same_character_in_same_anime_check_integrity_error_is_thrown(self):
+        Character.objects.create(name='Jet', description='Arm-less bounty hunter',
+                                 gender='M', anime=self.anime) 
+        self.assertRaises(
+            IntegrityError,
+            Character.objects.create,
+            name='Jet',
+            description='Another description',
+            gender='M',
+            anime=self.anime
+        )
